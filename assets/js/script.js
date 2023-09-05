5f702769262e686199402fef689fca022217db44
Ngozi89 <>
2023-09-05 23:06:15 +0000

    Set input to focus

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ const input = document.querySelector("input"),
     guess = document.querySelector(".guess"),
     checkButton = document.querySelector("button"),
     remainChances = document.querySelector(".chances");
+
+// Set input field
+input.focus();
+let randomNum = Math.floor(Math.random() * 100);
+chance = 10;
